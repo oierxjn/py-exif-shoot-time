a1296461848ec6ab0d5cb024ca372cbc1704e034
oierxjn <119778255+oierxjn@users.noreply.github.com>
2024-03-23 19:31:22 +0800

    Add files via upload
    修改BUG

--- ExifShootTime.py
+++ ExifShootTime.py
@@ -9,12 +9,12 @@
 #   date       2020-07-15 15:56:21
 #   face       (~￣▽￣)~
 #   weather    Tiantai Cloudy 32℃
-import os
+import os,sys
 import subprocess
 from dateutil.parser import parse
 from tkinter import *
 import tkinter.filedialog
-import win32gui
+import win32gui,win32con
 
 # ================ 公共类\方法 ================
 def fetchUserDateTime():
@@ -33,25 +33,33 @@ def fetchUserPicPaths(picExts):
     root = tkinter.Tk()
     root.withdraw()
     picExtsStr = " ".join(picExts)
-    return tkinter.filedialog.askopenfilenames(initialdir=(os.path.expanduser("~\Downloads")), filetypes=[("All Files", picExtsStr), ("PNG", ".png"), ("JPEG", ".jpg .jpeg .jfif .jpe"), ("BMP", ".bmp"), ("TIFF", ".tiff .tif"), ("GIF", ".gif")])
+    return tkinter.filedialog.askopenfilenames(initialdir=(os.path.expanduser(r"~\Downloads")), filetypes=[("All Files", picExtsStr), ("PNG", ".png"), ("JPEG", ".jpg .jpeg .jfif .jpe"), ("BMP", ".bmp"), ("TIFF", ".tiff .tif"), ("GIF", ".gif")])    
     
 def modifyShootTime(dateTime, filePathStr):
     print("\n\n")
-    cmdStr = os.getcwd() + '/resources/exiftool.exe -overwrite_original -DateTimeOriginal="' + str(dateTime) + '" ' + filePathStr
+    cmdStr = os.getcwd() + '/resources/exiftool.exe" -overwrite_original -DateTimeOriginal="' + str(dateTime) + '" ' + filePathStr
+    cmdStr = '"'+cmdStr
     child = subprocess.Popen(cmdStr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     sout, serr = child.communicate()
-    print(sout)
-    print(serr)
+#     print(cmdStr)
+    print("运行信息：",sout)
+    print("错误信息：",serr)
     
 def setFocus(title):
     if title == None:
         return
-    def callback (hwnd, hwnds):
-        if win32gui.IsWindow(hwnd) and win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
-            if win32gui.GetWindowText(hwnd) == title:
-                win32gui.SetForegroundWindow(hwnd)
-                return 
-    win32gui.EnumWindows(callback, [])
+#     def callback (hwnd, hwnds):
+#         if win32gui.IsWindow(hwnd) and win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
+#             if win32gui.GetWindowText(hwnd) == title:
+#                 win32gui.SetForegroundWindow(hwnd)
+#                 return 
+#     win32gui.EnumWindows(callback, [])
+    hwnd = win32gui.FindWindow(None, title)
+    if hwnd:
+        win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+        win32gui.SetForegroundWindow(hwnd)
+    else:
+        print("未找到指定的控制台窗口，请手动点击窗口")
 # ================ 公共类\方法 ================
 
 
@@ -66,25 +74,44 @@ if __name__=="__main__":
     # ---------------- 获取图片路径 ----------------
     picExts = [".png", ".jpg", ".jpeg", ".jfif", ".jpe", ".bmp", ".tiff", ".tif", ".gif"]
     print("您选择的文件是:")
-    filePathStr = ''
+    filePathStr_list = []
+    block_size=100
     if len(sys.argv) > 1:
+        list_cnt=0
+        file_cnt=0
+        filePathStr_list.append('')
         for filePath in sys.argv[1:]:
             if not os.path.isfile(filePath):
                 continue
             if not os.path.splitext(filePath)[1] in picExts:
                 continue
             print("\t", filePath)
-            filePathStr += '"' + filePath + '" '
+            filePathStr_list[list_cnt] += '"' + filePath + '" '
+            file_cnt+=1
+            if file_cnt==block_size:
+                filePathStr_list.append('')
+                list_cnt+=1
+                file_cnt=0
             
-    if len(filePathStr) == 0:
+    if len(filePathStr_list) == 0:
         filePaths = fetchUserPicPaths(picExts)
         if len(filePaths) == 0:
             print("您没有选择任何文件")
+            print("任意键退出")
             exit(1)
+        list_cnt=0
+        file_cnt=0
+        filePathStr_list.append('')
         for i in range(0, len(filePaths)):
             filePath = filePaths[i]
             print("\t", filePath)
-            filePathStr += '"' + filePath + '" '
+            filePathStr_list[list_cnt] += '"' + filePath + '" '
+            file_cnt+=1
+            if file_cnt==block_size:
+                filePathStr_list.append('')
+                list_cnt+=1
+                file_cnt=0
+                
     setFocus(title)
     print("\n\n")
     
@@ -92,11 +119,20 @@ if __name__=="__main__":
     # ---------------- 获取自定义日期 ----------------
     dateTime = fetchUserDateTime()
     print("您设定的日期是:\n\t", dateTime)
-    print("\n\n确认参数后按任意键继续")
+    print("\n\n确认参数后按任意键开始任务")
     os.system('pause>nul')
+    print("\n\n任务进行中\n\n")
     
+    # ---------------- 分块执行 ----------------
+    list_cnt=1
+    list_number=len(filePathStr_list)
+    for filePathStr in filePathStr_list:
+#         print(filePathStr)
+        modifyShootTime(dateTime, filePathStr)
+        print("第",list_cnt,"/",list_number,"分块完成")
+        list_cnt+=1
     
-    # ---------------- 执行 ----------------
-    modifyShootTime(dateTime, filePathStr)
+    # ---------------- 结束 ----------------
+    print("\n\n任务完成\n\n任意键退出")
     os.system('pause>nul')
 # ================ 主逻辑 ================
